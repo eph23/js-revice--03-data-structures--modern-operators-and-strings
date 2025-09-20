8e9eb50ec4be97130fd7ab31993de573c173eb76
ephraim <ephraims.818@gmail.com>
2025-09-21 01:06:33 +0600

    JS-REVICE--COMPLETE-JS---DATS-STRUCTURES-MODERN-OPERATORS-STRINGS - 07. Logical Assignment Operators

---
+++ 07. Logical Assignment Operators/script.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  // Destructuring returned value
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  // Destructuring method parameters
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]}, ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  // Spreading method parameters
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient, otherIngredients);
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+//
+
+const restaurant1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const restaurant2 = {
+  name: 'La Pizza',
+  owner: 'Giovanni Rossi',
+};
+
+// ||= → OR Assignment (Fill in if empty or falsey.)
+
+/* restaurant1.numGuests ||= 10;
+restaurant2.numGuests ||= 10;
+console.log(restaurant1);
+console.log(restaurant2); */
+
+// ??= → Nullish Assignmen (Assign only if missing (null/undefined))
+restaurant1.numGuests ??= 10;
+restaurant2.numGuests ??= 10;
+console.log(restaurant1);
+console.log(restaurant2);
+
+// &&= → AND Assignment (Update only if already valid.)
+restaurant1.owner &&= '<ANONYMOUS>';
+restaurant2.owner &&= '<ANONYMOUS>';
+console.log(restaurant1);
+console.log(restaurant2);

a2aaaba945835c1397b73340fe6178a36aa7afbe
ephraim <ephraims.818@gmail.com>
2025-09-23 20:49:19 +0600

    JS-REVICE--COMPLETE-JS---DATS-STRUCTURES-MODERN-OPERATORS-STRINGS - 10. Optional Chaining

---
+++ 10. Optional Chaining/script.js
@@ -0,0 +1,71 @@
+'use strict';
+const weekDays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekDays[3]]: {
+    open: 12,
+    close: 22,
+  },
+
+  fri: {
+    open: 11,
+    close: 23,
+  },
+
+  [`day-${2 + 2}`]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours,
+
+  // Destructuring returned value
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  // Destructuring method parameters
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]}, ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  // Spreading method parameters
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient, otherIngredients);
+  },
+};
+
+// Optional Chaining (?.)
+console.log(restaurant.openingHours?.mon?.open);
+console.log(restaurant.openingHours?.fri?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+console.log(restaurant.order?.(0, 1) ?? 'Method dose not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method dose not exists');
+
+const users = [{ name: 'Eph', email: 'info@eph.com' }];
+
+console.log(users[0]?.name ?? 'User array empty');
+console.log(users[1]?.name ?? 'User array empty');

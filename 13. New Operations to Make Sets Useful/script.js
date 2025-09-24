0fe770e8a8498e372a0fdcfdd6f681e2c6c273bb
ephraim <ephraims.818@gmail.com>
2025-09-24 20:53:26 +0600

    JS-REVICE--COMPLETE-JS---DATS-STRUCTURES-MODERN-OPERATORS-STRINGS - 13. New Operations to Make Sets Unique

---
+++ 13. New Operations to Make Sets Useful/script.js
@@ -0,0 +1,97 @@
+'use strict';
+const weekDays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekDays[3]]: {
+    open: 12,
+    close: 22,
+  },
+
+  fri: {
+    open: 11,
+    close: 23,
+  },
+
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours,
+
+  // Destructuring returned value
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  // Destructuring method parameters
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]}, ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  // Spreading method parameters
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient, otherIngredients);
+  },
+};
+
+// Sets
+const italianFoods = new Set([
+  'pasta',
+  'gnocchi',
+  'tomatoes',
+  'olive oil',
+  'garlic',
+  'basil',
+]);
+
+const mexicanFoods = new Set([
+  'tortillas',
+  'beans',
+  'rice',
+  'tomatoes',
+  'avocado',
+  'garlic',
+]);
+
+// More Set methods
+// intersection
+const commonFoods = italianFoods.intersection(mexicanFoods);
+console.log('Intersection', commonFoods);
+console.log([...commonFoods]);
+
+//Union
+const italianMexicanFusion = italianFoods.union(mexicanFoods);
+console.log('Union', italianMexicanFusion);
+console.log([...new Set([...italianFoods, ...mexicanFoods])]);
+
+//Difference
+const uniqueItalianFoods = italianFoods.difference(mexicanFoods);
+console.log(uniqueItalianFoods);
+
+const uniqueMexicanFoods = mexicanFoods.difference(italianFoods);
+console.log(uniqueMexicanFoods);
+
+// Symmetric Difference
+const uniqueItalianMexicanFoods =
+  italianFoods.symmetricDifference(mexicanFoods);
+console.log(uniqueItalianMexicanFoods);
+
+// Disjoint
+console.log(italianFoods.isDisjointFrom(mexicanFoods));
